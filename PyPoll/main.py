c63e492d9f526f3ce9f5050e7f3ce6713b938eac
Ben Snyder <BenSnyder90@gmail.com>
2020-02-21 21:21:55 -0500

    Finished code for PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -13,16 +13,22 @@ Vote_Count = 0
 Candidate_Totals = []
 Candidate_List = []
 Votes_List = []
+Vote_Percentage = []
 
-
+#Creates a function that runs through the votes and assigns them to
+#the candidate they voted for
 def VotesForCandidate(candidate):
+    #Sets the voter count to 0
     Voters = 0
+
     for i in Candidate_Totals:
-       
+       #Looks through the list of votes and compares it to the given candidate
+       #If the names match, one vote is added to the count
         if i == candidate:
             Voters += 1
  
     return Voters
+
 
 #Create variable for file path to the datafile
 csvpath = os.path.join('Resources', 'election_data.csv')
@@ -59,34 +65,52 @@ for j,x in enumerate(Candidate_Totals):
             else:
                 Candidate_List.append(Candidate_Totals[j])
 
-            
+#Get the vote totals for a specific candidate and adds to a list            
 for x in Candidate_List:
     Votes_List.append(VotesForCandidate(x))
 
+#Gets the percentage of total votes for each candidate and stores it in a list
+for x in Votes_List:
+    Votes = round((x / Vote_Count) * 100,0)
+    Vote_Percentage.append(Votes)
 
-print(Votes_List)
+
+Winner = Votes_List[0]
+Winner_Name = Candidate_List[0]
+
+for x in range(len(Candidate_List)):
+    if Votes_List[x] > Winner:
+        Winner = Votes_List[x]
+        Winner_Name = Candidate_List[x]
+#print(Votes_List)
+#print(Vote_Percentage)
 
 #Prints out the analysis to the terminal
-#print("Election Results")
-#print("------------------------")
+print("Election Results")
+print("------------------------")
 print(f"Total Votes: {Vote_Count}")
-#print(f"Total Profits: ${round(Net_Total)}")
-#print(f"Average Change: ${round(average,2)}")
-#print(f"Greatest Increase in Profits: {greatest_profit_month} (${round(greatest_profit)}) ")
-#print(f"Greatest Decrease in Profits: {greatest_losses_month} (${round(greatest_losses)}) ")
+print("------------------------")
+for x in range(len(Candidate_List)):
+    print(f"{Candidate_List[x]}: {Vote_Percentage[x]}% ({Votes_List[x]}) ")
+print("------------------------")
+print(f"Winner: {Winner_Name}")
+print("------------------------")   
+
 
  
 # Open the file using "write" mode
-#output_path = os.path.join("PyBankAnalysis.txt")
+output_path = os.path.join("Output","PyPollAnalysis.txt")
 
-#f = open(output_path, 'w')
+f = open(output_path, 'w')
 
-#*f.write("Financial Analysis \n")
-#f.write("------------------------\n")
-#f.write(f"Total Months: {Date_Count}\n")
-#f.write(f"Total Profits: ${round(Net_Total)}\n")
-#f.write(f"Average Change: ${round(average,2)}\n")
-#f.write(f"Greatest Increase in Profits: {greatest_profit_month} (${round(greatest_profit)})\n")
-#f.write(f"Greatest Decrease in Profits: {greatest_losses_month} (${round(greatest_losses)})\n")
+f.write("Election Results \n")
+f.write("------------------------\n")
+f.write(f"Total Votes: {Vote_Count}\n")
+f.write("------------------------\n")
+for x in range(len(Candidate_List)):
+    f.write(f"{Candidate_List[x]}: {Vote_Percentage[x]}% ({Votes_List[x]})\n")
+f.write("------------------------\n")
+f.write(f"Winner: {Winner_Name}\n")
+f.write("------------------------\n") 
 
-#f.close()
+f.close()
